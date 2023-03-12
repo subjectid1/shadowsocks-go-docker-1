f6df7f5cc4de8d73cfb40b3c2ef8a74375face18
subjectid1 <subjectid1@yandex.com>
2023-03-12 09:24:22 +0800

    Signed-off-by: subjectid1 <subjectid1@yandex.com>

--- endpoint-start.sh
+++ endpoint-start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Starting Shadowsocks Server..."
 echo "Use password $PASSWORD"
-/shadowsocks-server -k $PASSWORD -p $PORT -m $METHOD
+/shadowsocks-server -k $PASSWORD -p $PORT -m $METHOD -s "0.0.0.0"
